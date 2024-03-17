7fac3d4ea02892012263ab8ee830c2d42d96c8d2
Nehasachan409 <nehasachan409@gmail.com>
2024-03-17 23:46:00 +0530

    add js project

---
+++ script.js
@@ -0,0 +1,8 @@
+const computerScore = document.querySelector(".compScore");
+const yourScore=document.querySelector(".yourScore");
+const items=document.querySelectorAll(".item");
+const YouWin=document.getElementsByClassName("youwin");
+const YouWinLost=document.getElementsByClassName("youLost");
+const rulesDisplay = document.querySelector(".popup");
+const nextButton = document.querySelector("#next");
+const rulesButton = document.querySelector(".button");
